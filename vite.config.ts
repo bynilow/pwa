d4f83cbfab540e00d3e6acac95d8f8d7e2030c76
bynilow <bynilow@mail.ru>
2025-02-05 19:51:51 +0500

    fix m dir

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { ManifestOptions, VitePWA } from 'vite-plugin-pwa'
-import manifest from './src/manifest.json'
+import manifest from './manifest.json'
 
 // https://vite.dev/config/
 export default defineConfig({
