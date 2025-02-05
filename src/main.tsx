36711582c84c39c6c34181a62add7bfb484fbea4
bynilow <bynilow@mail.ru>
2025-02-05 19:18:04 +0500

    changed sw folder

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+
 if (navigator.serviceWorker) {
   try {
-    navigator.serviceWorker.register('../sw.js');
+    navigator.serviceWorker.register('/sw.js');
   }
   catch (error) {
     console.log(error)
   }
 }
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
